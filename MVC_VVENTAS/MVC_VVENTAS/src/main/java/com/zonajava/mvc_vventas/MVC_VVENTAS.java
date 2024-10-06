c8761e557ce323bb467f87897db49a94dce0cc02
AngelineSHB <astefany.hb17@gmail.com>
2024-10-05 20:24:30 -0500

    Add files via upload

---
+++ MVC_VVENTAS/MVC_VVENTAS/src/main/java/com/zonajava/mvc_vventas/MVC_VVENTAS.java
@@ -0,0 +1,15 @@
+package com.zonajava.mvc_vventas;
+
+import Vista.VIngreso;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MVC_VVENTAS {
+
+    public static void main(String[] args) {
+        VIngreso l= new VIngreso();
+        l.setVisible(true);
+    }
+}
